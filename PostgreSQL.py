d7d55bea536e9735af08378c7673cf06691afe15
anastasy_klimanova <klimanova_anastasy@mail.ru>
2024-07-08 00:55:09 +0300

    fix bags

--- PostgreSQL.py
+++ PostgreSQL.py
@@ -6,80 +6,189 @@ config = configparser.ConfigParser()
 config.read('settings.ini')
 password = config['Postgres']['password']
 
+###раскомментировать нужное при вызове функций
+# new_name = str(input("Обязательные поля для заполнения*\nПожалуйста, укажите имя* "))
+# new_surname = str(input("Пожалуйста, укажите фамилию*: "))
+# new_email = str(input("Введите email*: "))
+# new_phone_number = str(input("Введите номер(а) телефона(ов): "))
+# new_birthday = input("Пожалуйста, укажите дату рождения в формате YYYY-MM-DD ")
+
+
+
+#удаление таблиц
+def delete_table():
+    with conn.cursor() as cur:
+        cur.execute("""
+            DROP TABLE phone;
+            DROP TABLE client;
+        """)
+    return "Tables deleted"
+
+# создание БД
+def do_db():
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS client (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(40) NOT NULL,
+            surname VARCHAR(60) NOT NULL,
+            birthday DATE,
+            email VARCHAR(80) NOT NULL,
+            phone_number VARCHAR(100)
+        );
+    """)
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS phone (
+            id INTEGER PRIMARY KEY REFERENCES client(id),
+            phone_number VARCHAR(100)
+        );
+    """)
+    return "Database created"
+
+
+# заполним данные для удобства выполнения дальнейших запросов
+def create_tables():
+    cur.execute("""
+        INSERT INTO client(name, surname, birthday, email, phone_number)
+            VALUES('Анастасия', 'Климанова', '1998-04-01', '12345@mail.ru', '+79151234567'),
+                  ('Екатерина', 'Орлова', '1990-05-09', 'katka5@mail.ru', '+79261234567'),
+                  ('Ольга', 'Бузова', '1986-01-20', 'olka5@mail.ru', '+79251234567');
+    """)
+    cur.execute("""
+        INSERT INTO phone(id, phone_number)
+            VALUES('1', '300400500');
+    """)
+    return 'Tables created'
+
+# добавить нового клиента
+def new_client():
+    if len(new_name) <= 1:
+        print('Имя пользователя не заполнено')
+    if len(new_surname) <= 3:
+        print('Фамилия пользователя не заполнена')
+    if '@' not in new_email:
+        print('email пользователя не заполнен')
+    for _ in new_birthday:
+        try:
+            datetime.datetime.strptime(new_birthday, "%Y-%m-%d").date()
+            continue
+        except ValueError:
+            print('Пример заполнения даты рождения 1995-10-25, попробуйте еще раз')
+            break
+    cur.execute("""
+        INSERT INTO client(name, surname, birthday, email, phone_number)
+            VALUES(%s, %s, %s, %s, %s);
+    """, (new_name, new_surname, new_birthday, new_email, new_phone_number))
+    return 'Новый клиент внесен'
+
+
+
+# ищем id клиента
+def get_id(cursor, name: str) -> int:
+    cursor.execute("""
+        SELECT id FROM client WHERE name=%s;
+    """, (name,))
+    return cur.fetchone()[0]
+
+# добавить телефон для существующего клиента
+def add_phone(cursor, python_id):
+    cursor.execute("""
+        INSERT INTO phone(id, phone_number)
+            VALUES(%s, %s);
+    """, (python_id, new_phone_number))
+    return 'Номер успешно добавлен'
+
+# изменить старый номер на новый
+def change_phone(cursor, python_id):
+    cursor.execute("""
+        UPDATE client SET phone_number = %s WHERE id = %s;
+    """, (new_phone_number, python_id))
+    return 'Номер успешно изменен'
+
+# изменить данные о клиенте
+def change_client_data(cursor, python_id, new_name=None, new_surname=None, new_birthday=None, new_email=None):
+    cursor.execute("""
+        UPDATE client
+            SET name = %s, surname = %s, birthday = %s, email = %s
+            WHERE id = %s;
+    """, (new_name, new_surname, new_birthday, new_email, python_id))
+    return 'Данные клиента успешно изменены'
+
+def old_surname(cursor, python_id):
+    cur.execute("""
+        SELECT surname FROM client
+            WHERE id = %s;
+    """, (python_id,))
+    return cur.fetchone()
+
+def old_name(cursor, python_id):
+    cur.execute("""
+        SELECT name FROM client
+            WHERE id = %s;
+    """, (python_id,))
+    return cur.fetchone()
+
+def old_birthday(cursor, python_id):
+    cur.execute("""
+        SELECT birthday FROM client
+            WHERE id = %s;
+    """, (python_id,))
+    result = cur.fetchone()
+    formatted_date = result[0].strftime("%Y-%m-%d")
+    return formatted_date
+
+def old_email(cursor, python_id):
+    cur.execute("""
+        SELECT email FROM client
+            WHERE id = %s;
+    """, (python_id,))
+    return cur.fetchone()
+
+
+# удалить телефон для существующего клиента
+def delete_phone(cursor, python_id):
+    cursor.execute("""
+        UPDATE client
+            SET phone_number = %s
+            WHERE id = %s;
+    """, ('Номер удален', python_id))
+    return 'Номер удален'
+
+# удалить существующего клиента
+def delete_client(cursor, python_id):
+        cur.execute("""
+            DELETE FROM phone
+                WHERE id = %s;
+        """, (python_id,))
+        cur.execute("""
+            DELETE FROM client
+                WHERE id = %s;
+        """, (python_id,))
+        return 'Клиент удален'
+
+
+# найти клиента по его данным: имени, фамилии, email или телефону или дате рождения
+def find_client(cursor, name=None, surname=None, email=None, phone_number=None, birthday=None):
+    cursor.execute("""
+        SELECT id FROM client
+            WHERE name = %s OR surname = %s OR email = %s OR phone_number = %s OR birthday = %s;
+    """, (name, surname, email, phone_number, birthday))
+    return cur.fetchone()[0]
+
+#найти по номеру телефона в резервной таблице
+def find_phone_additional(cursor, phone_number):
+    cursor.execute("""
+        SELECT id FROM phone
+            WHERE phone_number = %s;
+    """, (phone_number,))
+    return cur.fetchone()[0]
 
 
 with psycopg2.connect(database='client_management_db', user='postgres', password=password) as conn:
     with conn.cursor() as cur:
-        def delete_table():
-            with conn.cursor() as cur:
-                cur.execute("""
-                    DROP TABLE phone;
-                    DROP TABLE client;
-                """)
-            return "Tables deleted"
-
-    # создание БД
-        def do_db():
-            with conn.cursor() as cur:
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS client (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(40) NOT NULL,
-                        surname VARCHAR(60) NOT NULL,
-                        birthday DATE NOT NULL,
-                        email VARCHAR(80) NOT NULL,
-                        phone_number VARCHAR(100)
-                    );
-                """)
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS phone (
-                        id INTEGER PRIMARY KEY REFERENCES client(id),
-                        phone_number_update VARCHAR(100)
-                    );
-                """)
-                conn.commit()
-            return "Database created"
-
-        #заполним данные для удобства выполнения дальнейших запросов
-        cur.execute("""
-            INSERT INTO client(name, surname, birthday, email, phone_number)
-                VALUES('Анастасия', 'Климанова', '1998-04-01', '12345@mail.ru', '+79151234567'),
-                      ('Екатерина', 'Орлова', '1990-05-09', 'katka5@mail.ru', '+79261234567'),
-                      ('Ольга', 'Бузова', '1986-01-20', 'olka5@mail.ru', '+79251234567');
-        """)
-        conn.commit()
-
-        cur.execute("""
-            INSERT INTO phone(id, phone_number_update)
-                VALUES('1', '300400500');
-        """)
-        conn.commit()
-
-    #добавить нового клиента
-        def new_client():
-            with conn.cursor() as cur:
-                new_name = str(input("Обязательные поля для заполнения*\nПожалуйста, укажите имя* "))
-                if len(new_name) <= 1:
-                    print('Имя пользователя не заполнено')
-                new_surname = str(input("Пожалуйста, укажите фамилию*: "))
-                if len(new_surname) <= 3:
-                    print('Фамилия пользователя не заполнена')
-                new_birthday = input("Пожалуйста, укажите дату рождения в формате YYYY-MM-DD ")
-                new_birthday = datetime.datetime.strptime(new_birthday, "%Y-%m-%d")
-                #print(new_birthday) не понимаю, почему при принте он выводит норм дату(хоть и со временем 00:00:00),
-                #а в выводе в консоли все равно дает datetime.date(тут цифры через запятую)
-                new_email = str(input("Введите email*: "))
-                if '@' not in new_email:
-                    print('email пользователя не заполнен')
-                new_phone_number = str(input("Введите номер(а) телефона(ов): "))
-                cur.execute("""
-                    INSERT INTO client(name, surname, birthday, email, phone_number)
-                        VALUES(%s, %s, %s, %s, %s);
-                """, (new_name, new_surname, new_birthday, new_email, new_phone_number))
-
-                conn.commit()
-            return 'Новый клиент внесен'
-
+        print(delete_table())
+        print(do_db())
+        print(create_tables())
+        # print(new_client())
         # cur.execute("""
         #      SELECT * FROM client;
         # """)
@@ -89,142 +198,16 @@ with psycopg2.connect(database='client_management_db', user='postgres', password
         #              SELECT * FROM phone;
         #         """)
         # print('вся таблица телефон', cur.fetchall())
+        # print(get_id(cur, 'Анастасия'))
+        # print(add_phone(cur, 2))
+        # print(change_phone(cur, 3))
+        #### если хотим поменять, например, только почту: new_email='123@ya.com', остальные параметры с функцией old_[название переменной](cur, id) и т.д.
+        # print(change_client_data(cur, 2, new_name=old_name(cur, 2), new_surname=old_surname(cur, 2), new_birthday=old_birthday(cur, 2), new_email='123@ya.com'))
+        # # print(delete_phone(cur, 1))
+        # print(delete_client(cur, '2'))
+        # print(find_client(cur, birthday='1998-04-01'))
+        # print(find_phone_additional(cur, '300400500'))
 
-    #ищем id клиента
-        def get_id(cursor, name: str) -> int:
-            cursor.execute("""
-                SELECT id FROM client WHERE name=%s;
-            """, (name,))
-            return cur.fetchone()[0]
-
-        python_id = get_id(cur, 'Екатерина')
-
-
-
-    #добавить телефон для существующего клиента
-        def add_phone(cursor, python_id):
-            new_phone = input('Введите номер телефона: ')
-            cursor.execute("""
-                INSERT INTO phone(id, phone_number_update)
-                    VALUES(%s, %s);
-            """, (python_id, new_phone))
-            conn.commit()
-            return 'Номер успешно добавлен'
-
-        # phone_check_second_table = add_phone(cur, 2)
-
-
-    #изменить старый номер на новый
-        def change_phone(cursor, python_id):
-            cursor.execute("""
-                UPDATE client SET phone_number = %s WHERE id = %s;
-            """, (input('Введите новый номер: '), python_id))
-            conn.commit()
-            return 'Номер успешно изменен'
-
-        # check_change_phone = change_phone(cur, 2)
-
-
-    # изменить данные о клиенте
-        def change_client_data(cursor, python_id):
-            cursor.execute("""
-            UPDATE client 
-                SET name = %s, surname = %s, birthday = %s, email = %s
-                WHERE id = %s;
-            """, (input('Введите имя: '),
-                  input('Введите фамилию: '),
-                  input('Введите дату рождения в формате YYYY-MM-DD: '),
-                  input('Введите email: '),
-                  python_id))
-            conn.commit()
-            return 'Данные клиента успешно изменены'
-
-        # check_change_client_data = change_client_data(cur, 2)
-
-    #удалить телефон для существующего клиента
-        def delete_phone(cursor, python_id):
-            cursor.execute("""
-            UPDATE client
-                SET phone_number = %s
-                WHERE id = %s;
-            """, ('Номер удален', python_id))
-            conn.commit()
-            return 'Номер удален'
-
-        # check_delete_phone = delete_phone(cur, 2)
-
-    #удалить существующего клиента
-        def delete_client(python_id):
-            with conn.cursor() as cur:
-                cur.execute("""
-                DELETE FROM phone
-                    WHERE id = %s;
-                """, (python_id,))
-                cur.execute("""
-                DELETE FROM client
-                    WHERE id = %s;
-                """, (python_id,))
-                conn.commit()
-                return 'Клиент удален'
-
-        check_delete_client = delete_client(3)
-
-    #найти клиента по его данным: имени, фамилии, email или телефону
-        def find_client(cursor, name, surname):
-            cursor.execute("""
-            SELECT id FROM client
-                WHERE
-                name = %s AND 
-                surname = %s 
-            """, (name, surname))
-            conn.commit()
-            return cur.fetchone()[0]
-
-        def find_client_email(cursor, email):
-            cursor.execute("""
-            SELECT id FROM client
-                WHERE
-                email = %s
-            """, (email,))
-            conn.commit()
-            return cur.fetchone()[0]
-
-        def find_client_phone(cursor, phone_number):
-            cursor.execute("""
-                SELECT id FROM client
-                    WHERE
-                    phone_number = %s
-            """, (phone_number,))
-            conn.commit()
-            return cur.fetchone()[0]
-        
-        def find_client_phone_2(cursor, phone_number_update):
-            cursor.execute("""
-                SELECT id FROM phone
-                    WHERE
-                    phone_number_update = %s
-            """, (phone_number_update,))
-            conn.commit()
-            return cur.fetchone()[0]
-        
-        # check_find = find_client(cur,'Анастасия', 'Климанова')
-        # find_cl_email = find_client_email(cur, '12345@mail.ru')
-        # find_cl_phone1 = find_client_phone(cur, '+79261234567')
-        # find_cl_phone2 = find_client_phone_2(cur,'300400500')
-        
-if __name__ == '__main__':
-    print(delete_table())
-    print(do_db())
-    # print(new_client())
-    # print('id', python_id)
-    # print(phone_check_second_table)
-    # print(check_change_phone)
-    # print(check_change_client_data)
-    # print(check_delete_phone)
-    # print(check_delete_client)
-    # print(check_find)
-    # print(find_cl_email)
-    # print(find_cl_phone1)
-    # print(find_cl_phone2)
+conn.close()
 
 
